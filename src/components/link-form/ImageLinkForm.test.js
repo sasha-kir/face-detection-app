b2f56cb3ab3bb594192c858e2fac55f32e676a7e
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-09-04 20:47:25 +0300

    modified ImageLinkForm tests

--- src/components/link-form/ImageLinkForm.test.js
+++ src/components/link-form/ImageLinkForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import { positions, Provider as AlertProvider } from "react-alert";
+import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from '../alert-template/react-alert-custom-template';
 
 import ImageLinkForm from './ImageLinkForm';
@@ -10,26 +10,31 @@ const inputFunc = jest.fn();
 const submitFunc = jest.fn();
 
 describe('ImageLinkForm', () => {
-  it('renders correctly', () => {
-    const component = shallow(<ImageLinkForm onInputChange={inputFunc} onImageSubmit={submitFunc} />);
-    expect(component).toMatchSnapshot();
-  });
+   let component;
 
-  it('sends input to App', () => {
-    const component = mount(<ImageLinkForm onInputChange={inputFunc} 
-                                             onImageSubmit={submitFunc} />);
-    component.find('input').simulate('change');
-    expect(inputFunc).toHaveBeenCalled();
-    component.unmount();
-  });
-
-  it('handles valid url', () => {
-    const component = mount(
+   beforeEach(() => {
+    component = mount(
         <AlertProvider template={AlertTemplate}>
             <ImageLinkForm onInputChange={inputFunc} 
                            onImageSubmit={submitFunc} />
         </AlertProvider>);
+  });
+  
+  afterEach(() => {
+    component.unmount();
+  });
 
+  it('renders correctly', () => {
+    const wrapper = shallow(<ImageLinkForm onInputChange={inputFunc} onImageSubmit={submitFunc} />)
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('sends input to App', () => {
+    component.find('input').simulate('change');
+    expect(inputFunc).toHaveBeenCalled();
+  });
+
+  it('handles valid url', () => {
     const form = component.find('ImageLinkForm');
     
     const urls = [
@@ -44,19 +49,11 @@ describe('ImageLinkForm', () => {
         urlInput.instance().value = url;
         form.find('button#fwd-btn').simulate('click');
         expect(form.state('isUrlValid')).toBeTruthy();
-    }
-
-    component.unmount();
+    };
 
   });
 
   it('handles invalid url', () => {
-    const component = mount(
-        <AlertProvider template={AlertTemplate}>
-            <ImageLinkForm onInputChange={inputFunc} 
-                           onImageSubmit={submitFunc} />
-        </AlertProvider>);
-
     const form = component.find('ImageLinkForm');
     
     const urls = [
@@ -72,33 +69,21 @@ describe('ImageLinkForm', () => {
         form.find('button#fwd-btn').simulate('click');
         expect(form.state('isUrlValid')).toBeFalsy();
     }
-
-    component.unmount();
   });
 
   it('submits url on button click', () => {
-    const component = mount(<ImageLinkForm onInputChange={inputFunc} 
-                                           onImageSubmit={submitFunc} />);
-
     const urlInput = component.find('input').at(0);
     urlInput.instance().value = "http://example.com/image.jpg";
+    component.find('button#fwd-btn').simulate('click');
 
-    component.find('button').simulate('click');
-    
     expect(submitFunc).toHaveBeenCalled();
-    component.unmount();
   });
 
   it('submits url on Enter', () => {
-    const component = mount(<ImageLinkForm onInputChange={inputFunc} 
-                                           onImageSubmit={submitFunc} />);
-
     const urlInput = component.find('input').at(0);
     urlInput.instance().value = "http://example.com/image.jpg";
-
     component.find('input').simulate('keydown', { keyCode: 13 });
     
     expect(submitFunc).toHaveBeenCalled();
-    component.unmount();
   });
 });
