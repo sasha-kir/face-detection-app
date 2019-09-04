c28a36f2308303374bce89a158f3a5583ae1dc15
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-09-04 21:50:49 +0300

    added tests for FaceDetection

--- src/components/link-form/ImageLinkForm.test.js
+++ src/components/link-form/ImageLinkForm.test.js
@@ -4,7 +4,6 @@ import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from '../alert-template/react-alert-custom-template';
 
 import ImageLinkForm from './ImageLinkForm';
-import { url } from 'inspector';
 
 const inputFunc = jest.fn();
 const submitFunc = jest.fn();
@@ -45,7 +44,7 @@ describe('ImageLinkForm', () => {
 
     const urlInput = component.find('input').at(0);
 
-    for (url of urls) {
+    for (var url of urls) {
         urlInput.instance().value = url;
         form.find('button#fwd-btn').simulate('click');
         expect(form.state('isUrlValid')).toBeTruthy();
@@ -64,7 +63,7 @@ describe('ImageLinkForm', () => {
 
     const urlInput = component.find('input').at(0);
 
-    for (url of urls) {
+    for (var url of urls) {
         urlInput.instance().value = url;
         form.find('button#fwd-btn').simulate('click');
         expect(form.state('isUrlValid')).toBeFalsy();
